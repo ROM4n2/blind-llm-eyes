330d2ab9ecf982cf6f03a73cf1b110e2078123f3
ROM4n2 <beianderen@gmail.com>
2026-08-12 14:37:10 +0800

    chore: init blind-llm-eyes project skeleton

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ROM4n2/blind-llm-eyes
+
+go 1.26.5
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("blind-llm-eyes: starting...")
+	// TODO: load config, start server
+}
